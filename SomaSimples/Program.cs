f4f3ac2dccf92b64e8fcf33f4008e82afcbe8646
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:22:57 -0300

    Adicionar arquivos de projeto.

---
+++ SomaSimples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+
+                int x, y, soma;
+
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+
+                soma = x + y;
+
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
